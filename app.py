d1d589ca724d29530f9d095a8e623143236f1131
bhavani <iamdurgabhavani@gmail.com>
2024-09-19 00:42:21 -0700

    it is a line

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
  This program prints Hello, world!
 
+<<<<<<< HEAD
 print('Hello, shiva!')
 print ("hello mahi")
+=======
+print('Hello, team!')
+print ("hello class")
+>>>>>>> parent of 1b07512 (Update app.py students)
