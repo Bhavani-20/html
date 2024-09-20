022ab8ed7275bb6249d717ac6236b8ce2cb6c0ba
bhavani <iamdurgabhavani@gmail.com>
2024-09-19 22:41:27 -0700

    PR

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ print('Hello, shiva!')
 print ("hello mahi")
 =======
 print('Hello, team!')
-print ("hello class")
+print ("hello we are working on PR")
 >>>>>>> parent of 1b07512 (Update app.py students)
